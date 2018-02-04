9d9c815fe2a81eff864a683139930fbc8ad26a33
Alan Dayton <alan.dayton.1@gmail.com>
2018-02-03 21:56:30 -0700

    Added function to get the vectors associated with all the words in a file

--- src/word_to_vector.py
+++ src/word_to_vector.py
@@ -5,7 +5,8 @@ import spacy
 # Load English word vectors
 nlp = spacy.load('en_vectors_web_lg')
 
-def WordToVector(word):
+
+def word_to_vector(word):
     """
     Description
     -----------
@@ -28,7 +29,8 @@ def WordToVector(word):
     # Return the word vector
     return doc.vector
 
-def WordListToVectors(filename):
+
+def word_list_to_vectors(filename):
     """
     Description
     -----------
@@ -65,12 +67,49 @@ def WordListToVectors(filename):
     # Return a list of all the word vectors
     return word_vectors
 
+
+def file_to_vectors(filename):
+    """
+    Description
+    -----------
+        Gets the vectors associated with each word in a file (word2vec).
+
+    Parameters
+    ----------
+        filename : str
+            The path to the file containing the words to convert to vector form
+
+    Returns
+    -------
+        word_vectors : list
+            A list of all the vectors associated with each word in the input file
+    """
+
+    # Store each word vector
+    word_vectors = []
+
+    # Process the whole text file
+    text = open(filename).read()
+    doc = nlp(text)
+
+    # Save the word vector for each
+    for token in doc:
+        word_vectors.append(token.vector)
+
+    # Return a list of all the word vectors
+    return word_vectors
+
+
 if __name__ == '__main__':
     # Test for converting a single word to a vector
     word = 'airplane'
-    word_vector = WordToVector(word)
+    word_vector = word_to_vector(word)
     print(word_vector)
 
     # Test for converting a list of words in a file to a list of word vectors
     filename = os.path.join(os.path.dirname(__file__), '../data/quickdraw_categories.txt')
-    word_vectors = WordListToVectors(filename)
+    word_vectors = word_list_to_vectors(filename)
+
+    # Test for converting a file to a list of word vectors
+    filename = os.path.join(os.path.dirname(__file__), '../data/peter_rabbit.txt')
+    word_vectors = file_to_vectors(filename)
