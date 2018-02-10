4f8fc5bfb18df7e10c1184d12bcbfc90486fe576
Alan Dayton <alan.dayton.1@gmail.com>
2018-02-10 15:23:12 -0700

    Modified the auto detection script to save the edge images and optionally show them

--- src/auto_edge_detection.py
+++ src/auto_edge_detection.py
@@ -1,4 +1,5 @@
 # Source: https://www.pyimagesearch.com/2015/04/06/zero-parameter-automatic-canny-edge-detection-with-python-and-opencv/
+# Some modifications made to save the images
 
 # import the necessary packages
 import numpy as np
@@ -23,6 +24,8 @@ def auto_canny(image, sigma=0.33):
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--images", required=True, help="path to input dataset of images")
+ap.add_argument("-d", "--destination", required=True, help="path to save edge images")
+ap.add_argument("-s", "--show", action="store_true", help="show each image")
 args = vars(ap.parse_args())
 
 # loop over the images
@@ -32,13 +35,19 @@ for imagePath in glob.glob(args["images"] + "/*.jpg"):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     blurred = cv2.GaussianBlur(gray, (3, 3), 0)
 
-    # apply Canny edge detection using a wide threshold, tight
-    # threshold, and automatically determined threshold
-    wide = cv2.Canny(blurred, 10, 200)
-    tight = cv2.Canny(blurred, 225, 250)
+    # apply Canny edge detection using a wide threshold, tight threshold, and automatically determined threshold
+    # wide = cv2.Canny(blurred, 10, 200)
+    # tight = cv2.Canny(blurred, 225, 250)
     auto = auto_canny(blurred)
 
+    # save the image
+    filename = imagePath[len(args["images"]) + 1:]
+    destination_path = args["destination"] + "/" + filename
+    cv2.imwrite(destination_path, auto)
+
     # show the images
-    cv2.imshow("Original", image)
-    cv2.imshow("Edges", np.hstack([wide, tight, auto]))
-    cv2.waitKey(0)
+    if args["show"]:
+        cv2.imshow("Original", image)
+        cv2.imshow("Edges", np.hstack([gray, auto]))
+        # cv2.imshow("Edges", np.hstack([wide, tight, auto]))
+        cv2.waitKey(0)
