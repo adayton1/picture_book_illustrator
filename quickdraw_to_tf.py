ef061cf8d133275f1eea194dbe232f14e61b2b6b
William Myers <mwilliammyers@gmail.com>
2018-02-28 18:01:55 -0700

    Remove tf implementation of quickdraw dataset converter

--- quickdraw_to_tf.py
+++ quickdraw_to_tf.py
@@ -24,38 +24,6 @@ def bytes_feature(values):
 	return tf.train.Feature(bytes_list=tf.train.BytesList(value=[values]))
 
 
-def load_data_tensorflow(src_path=data_root + '/quickdraw'):
-	dest_path = '{}/quickdraw_tf/train{}.tfrecords'.format(data_root, '_{}'.format(limit) if limit else '')
-	writer = tf.python_io.TFRecordWriter(dest_path)
-
-	with tf.Graph().as_default():
-		image_placeholder = tf.placeholder(dtype=tf.uint16)
-		encoded_image = tf.image.encode_png(image_placeholder, compression=3)
-
-		config = tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True))
-		# config = tf.ConfigProto(device_count={'GPU': 0, 'CPU': 10}, allow_soft_placement=True,
-		# 						log_device_placement=True)
-		with tf.Session('', config=config) as sess:
-			for label_id, filename in enumerate(tqdm(os.listdir(src_path))):
-				raw = np.load(os.path.join(src_path, filename))
-				sample = raw[np.random.choice(raw.shape[0], limit, replace=False), :] if limit else raw
-				for image in sample:
-					image = np.reshape(image, (image_height, image_width, image_num_channels))
-					img = sess.run(encoded_image, feed_dict={image_placeholder: image})
-
-					feature = {
-						'feature/encoded': bytes_feature(img),
-						'feature/format': bytes_feature(image_format),
-						'label/encoded': int64_feature(label_id),
-						'label/human': bytes_feature(tf.compat.as_bytes(os.path.splitext(filename)[0])),
-						'feature/height': int64_feature(image_height),
-						'feature/width': int64_feature(image_width),
-					}
-
-					example = tf.train.Example(features=tf.train.Features(feature=feature))
-					writer.write(example.SerializeToString())
-
-
 def load_data(src_path=data_root + '/quickdraw'):
 	dest_path = '{}/quickdraw_tf/train{}.tfrecords'.format(data_root, '_{}'.format(limit) if limit else '')
 	writer = tf.python_io.TFRecordWriter(dest_path)
