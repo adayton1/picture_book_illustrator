e388c2d91a577557b57046c96f0df55763088e03
Alan Dayton <alan.dayton.1@gmail.com>
2018-03-22 08:53:24 -0600

    Added in style transfer, works for the first image, but breaks after that

--- illustrator/illustrator.py
+++ illustrator/illustrator.py
@@ -4,6 +4,11 @@ import os
 import subprocess
 import spacy
 
+import tensorflow as tf
+import numpy as np
+from deps.faststyle.im_transf_net import create_net
+import deps.faststyle.utils as utils
+
 # Load English model
 nlp = spacy.load('en_core_web_lg')
 
@@ -18,8 +23,41 @@ def read_file(input_file):
     return text, pages
 
 
-def illustrate(input_file, output_dir):
+# Adapted from https://github.com/ghwatson/faststyle/blob/master/stylize_image.py
+def stylize_image(input_img_path, output_img_path, model_path, upsample_method='resize', content_target_resize=1.0):
+    print('Stylizing image...')
 
+    # Read + preprocess input image.
+    img = utils.imread(input_img_path)
+    img = utils.imresize(img, content_target_resize)
+    img_4d = img[np.newaxis, :]
+
+    # Create the graph.
+    with tf.variable_scope('img_t_net'):
+        X = tf.placeholder(tf.float32, shape=img_4d.shape, name='input')
+        Y = create_net(X, upsample_method)
+
+    # Saver used to restore the model to the session.
+    saver = tf.train.Saver()
+
+    # Filter the input image.
+    with tf.Session() as sess:
+        print('Loading up model...')
+        saver.restore(sess, model_path)
+        print('Evaluating...')
+        img_out = sess.run(Y, feed_dict={X: img_4d})
+
+    # Postprocess + save the output image.
+    print('Saving image...')
+    img_out = np.squeeze(img_out)
+    utils.imwrite(output_img_path, img_out)
+
+    print('Done stylizing image.')
+
+
+def illustrate(input_file, output_dir, style_model):
+
+    print("Reading input file...")
     text, pages = read_file(input_file)
 
     # Process the whole doc
@@ -27,6 +65,9 @@ def illustrate(input_file, output_dir):
 
     # Iterate through each page
     for i, page in enumerate(pages):
+        print("Illustrating page {0}...".format(i))
+
+        print("Natural language processing...")
         page_doc = nlp(page)
 
         nouns = []
@@ -38,6 +79,7 @@ def illustrate(input_file, output_dir):
                 nouns.append(noun.lemma_)
 
         # Download image
+        print("Downloading image...")
         keywords = "{0}".format(" ".join(nouns))
         subprocess.call(["googleimagesdownload", "-k", keywords, "-l", "1", "-o", output_dir, "-f", "jpg"])
 
@@ -53,6 +95,11 @@ def illustrate(input_file, output_dir):
         # Clear out the temporary folder
         subprocess.call(["rm", "-r", temp_dir])
 
+        # Stylize image
+        stylize_image(destination, destination, style_model, i)
+        #subprocess.call(["python", "stylize_image.py", "--input_img_path", destination, "--output_img_path",
+        #                "./out.jpg", "--model_path", "./models/starry_final.ckpt"])
+
 
 if __name__ == "__main__":
     # Imports
@@ -64,9 +111,12 @@ if __name__ == "__main__":
                         default=os.path.join(os.path.dirname(__file__), '../data/peter_rabbit.txt'))
     parser.add_argument('--output-dir', type=str, required=False, help='Path to the output directory.',
                         default=os.path.join(os.path.dirname(__file__), '../illustrated_books'))
+    parser.add_argument('--style-model', type=str, required=False, help='Path to the style transfer model.',
+                        default=os.path.join(os.path.dirname(__file__), '../deps/faststyle/models/starry_final.ckpt'))
     args = parser.parse_args()
 
     input_file = os.path.abspath(args.input_file)
     output_dir = os.path.abspath(args.output_dir)
+    style_model = os.path.abspath(args.style_model)
 
-    illustrate(input_file, output_dir)
+    illustrate(input_file, output_dir, style_model)
